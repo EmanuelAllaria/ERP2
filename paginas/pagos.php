27ea1d3e366795ed8d54f130d7e6412c26c5b9c2
VulturDev <vulturdevs@gmail.com>
2024-07-21 22:50:42 -0300

    agregado para que se pueda ver la imagen

--- paginas/pagos.php
+++ paginas/pagos.php
@@ -196,7 +196,13 @@
                                                   ORDER BY id ASC");
 
                       while ($row_cheque = mysqli_fetch_assoc($con_cheque)) {
-                        $imagen_cheque = $link->query("SELECT * FROM imagen_cheque WHERE numero_cheque = 546546464;")->fetch_assoc()['url_imagen'];
+                        $numero_cheque = $row_cheque['numero_cheque'];
+                        $result = $link->query("SELECT * FROM imagen_cheque WHERE numero_cheque = $numero_cheque");
+                        if ($result) {
+                          $imagen_cheque = $result->fetch_assoc();
+                          $imagen_cheque = $imagen_cheque ? $imagen_cheque['url_imagen'] : null;
+                        }
+
                         echo "<tr style='width:100%;display:none;' class='tr_cheques_$id_pago'>";
                         echo "<td>{$row_cheque['id_pago']}</td>";
                         echo "<td>{$row_cheque['numero_cheque']}</td>";
@@ -214,7 +220,8 @@
                           (intval($row_cheque['cheque_rechazado']) === 1
                             ? '<i title="Rechazado" class="fa-solid fa-circle" style="color:red;"></i>'
                             : '<i title="Aceptado" class="fa-solid fa-circle" style="color:green;"></i>') .
-                          " <a href='uploads/cheques/" . $imagen_cheque . "' target='_blank'><i class='fa-solid fa-image'></i></a></td>";
+                          " " . ($imagen_cheque ? "<a href='uploads/cheques/" . $imagen_cheque . "' target='_blank'><i class='fa-solid fa-image'></i></a>" : "") . "</td>";
+
                         echo "</tr>";
                       }
                     }
